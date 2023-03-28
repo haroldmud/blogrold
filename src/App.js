c2357f923477d3914f8406ea6ac6a2143567179c
haroldmud <haroldmudosa@gmail.com>
2023-03-28 18:06:44 +0300

    Feat: Create UI and UX for the API data

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Api from "./components/apiSample";
+import Header from "./components/Header";
+import Blog from "./components/Blog";
+
+function App() {
+  return (
+    <main>
+      <Header/>
+      <Blog/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+import {FaBlog} from 'react-icons/fa'
+import {CiSearch,CiBellOn} from 'react-icons/ci'
+import {TfiWrite} from 'react-icons/tfi'
+import {IoMdContact} from 'react-icons/io'
+import {SlArrowDown} from 'react-icons/sl'
+
+
+function Header() {
+  const [isScroll, setIsScroll] = useState(true)
+   useEffect(()=>{
+    const handleScroll =()=>{
+      if(window.scrollY > 500){
+        setIsScroll(false)
+      }else{
+        setIsScroll(true)
+      }
+    }
+    window.addEventListener("scroll", handleScroll)
+   })
+  
+  return (
+  <header className={` ${isScroll? "py-4" : "py-1"} h-fit fixed w-full bg-white px-4  flex justify-between border-b`}>
+        <div className="flex">
+          <a target="_blank" href="https://www.blogger.com" className='my-auto'><span className={`${isScroll ? "text-[2.3rem]": "text-[1.4rem]"} text-blue-900`}><FaBlog/></span></a>
+          <div className="my-auto ml-4">
+            <form className="md:flex hidden bg-gray-100 rounded-3xl my-auto p-1 pr-4">
+              <div className={`${isScroll ? "text-3xl" : "text-xl"} my-auto text-gray-400`}><CiSearch/></div>
+              <input type="text" className=" bg-gray-100 outline-none pl-2 placeholder:text-gray-400" placeholder="Search Blogrold" />
+            </form>
+          </div>
+        </div>
+        <div className="flex gap-8 pr-8">
+          <div className="md:hidden flex my-auto text-3xl text-gray-400"><CiSearch/></div>
+            <button className="text-gray-400 flex gap-1">
+              <span className="my-auto text-2xl text-gray-400"><TfiWrite/></span>
+              <p className="my-auto">Write</p>
+            </button>
+            <button className="h-fit my-auto">
+              <span className="my-auto text-3xl text-gray-400"><CiBellOn/></span>
+            </button>
+            <button className="flex">
+              <span className="my-auto text-3xl text-gray-400"><IoMdContact/></span>
+              <span className="my-auto text-[0.5rem] text-gray-400"><SlArrowDown/></span>
+            </button>
+        </div>
+      </header>
+  )
+}
+
+export default Header
